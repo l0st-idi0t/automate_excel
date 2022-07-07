e94d0308528fad875453eb42ee7204c94ea47a3b
Carson <carlnod121@gmail.com>
2022-07-07 18:59:12 -0500

    more testing

--- automate.py
+++ automate.py
@@ -17,19 +17,30 @@ wb = load_workbook('Customer General Code #2  6-28-2022.xlsx', data_only=True, r
 changeActive('Sheet2', wb)
 sheet = wb.active
 
+i = 2
+
 with open('test.txt', 'w') as f:
-	for i in range(2, 200):
+	while i <= 200:
+		count = 0
 		for j in range(1, 11):
 			one = sheet[f'E{i}'].value.replace('-', '').replace('_', '')
 			two = sheet[f'E{i+j}'].value.replace('-', '').replace('_', '')
 			onearr = ''.join([i for i in [char for char in one] if i.isdigit()])
 			twoarr = ''.join([i for i in [char for char in two] if i.isdigit()])
-			if fuzz.token_sort_ratio(one, two) >= 80 and fuzz.token_sort_ratio(onearr, twoarr) >= 85:
+			if fuzz.token_sort_ratio(one, two) >= 80 and fuzz.token_sort_ratio(onearr, twoarr) >= 85 or fuzz.token_sort_ratio(onearr, twoarr) >= 90:
 				f.write(f"{i}: {sheet[f'E{i}'].value}\n")
+				print(i)
+				count += 1
+			else:
+				f.write(f'{i}: NOT MATCH')
 
+		if count > 0:
+			i += count
+		else:
+			i += 1
 
-one = sheet['E13'].value.replace('-', '').replace('_', '')
-two = sheet['E14'].value.replace('-', '').replace('_', '')
+one = sheet['E7'].value.replace('-', '').replace('_', '')
+two = sheet['E8'].value.replace('-', '').replace('_', '')
 print(one, two)
 
 print(fuzz.token_sort_ratio(one, two))
