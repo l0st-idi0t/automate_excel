ae0f7887cdcc5457ca03c4e61a9ec403dd369f46
Carson <carlnod121@gmail.com>
2022-07-07 19:44:13 -0500

    testing + fixes

--- automate.py
+++ automate.py
@@ -19,28 +19,28 @@ sheet = wb.active
 
 i = 2
 
-with open('test.txt', 'w') as f:
-	while i <= 200:
-		count = 0
-		for j in range(1, 11):
-			one = sheet[f'E{i}'].value.replace('-', '').replace('_', '')
-			two = sheet[f'E{i+j}'].value.replace('-', '').replace('_', '')
-			onearr = ''.join([i for i in [char for char in one] if i.isdigit()])
-			twoarr = ''.join([i for i in [char for char in two] if i.isdigit()])
-			if fuzz.token_sort_ratio(one, two) >= 80 and fuzz.token_sort_ratio(onearr, twoarr) >= 85 or fuzz.token_sort_ratio(onearr, twoarr) >= 90:
-				f.write(f"{i}: {sheet[f'E{i}'].value}\n")
-				print(i)
-				count += 1
-			else:
-				f.write(f'{i}: NOT MATCH')
+# with open('test.txt', 'w') as f:
+# 	while i <= 2527:
+# 		count = 0
+# 		for j in range(1, 11):
+# 			one = sheet[f'E{i}'].value.replace('-', '').replace('_', '') if not sheet[f'E{i}'].value.split('-')[-1].isdigit() else sheet[f'E{i}'].value[:-4].replace('-', '').replace('_', '')
+# 			two = sheet[f'E{i+j}'].value.replace('-', '').replace('_', '') if not sheet[f'E{i+j}'].value.split('-')[-1].isdigit() else sheet[f'E{i+j}'].value[:-4].replace('-', '').replace('_', '')
+# 			onearr = ''.join([i for i in [char for char in one] if i.isdigit()])
+# 			twoarr = ''.join([i for i in [char for char in two] if i.isdigit()])
+# 			if fuzz.token_sort_ratio(one, two) >= 80 and fuzz.token_sort_ratio(onearr, twoarr) >= 85 or fuzz.token_sort_ratio(onearr, twoarr) >= 90:
+# 				f.write(f"{i}: {sheet[f'E{i}'].value}\n")
+# 				print(i)
+# 				count += 1
+# 			else:
+# 				break
 
-		if count > 0:
-			i += count
-		else:
-			i += 1
+# 		if count > 0:
+# 			i += count
+# 		else:
+# 			i += 1
 
-one = sheet['E7'].value.replace('-', '').replace('_', '')
-two = sheet['E8'].value.replace('-', '').replace('_', '')
+one = sheet[f'E52'].value.replace('-', '').replace('_', '') if not sheet[f'E52'].value.split('-')[-1].isdigit() else sheet[f'E52'].value[:-4].replace('-', '').replace('_', '')
+two = sheet[f'E53'].value.replace('-', '').replace('_', '') if not sheet[f'E53'].value.split('-')[-1].isdigit() else sheet[f'E53'].value[:-4].replace('-', '').replace('_', '')
 print(one, two)
 
 print(fuzz.token_sort_ratio(one, two))
