3d3bfaa2e08783dda33209bd7041d981472f91eb
Carson <carlnod121@gmail.com>
2022-07-07 19:57:43 -0500

    fixes + some optimization

--- automate.py
+++ automate.py
@@ -17,27 +17,25 @@ wb = load_workbook('Customer General Code #2  6-28-2022.xlsx', data_only=True, r
 changeActive('Sheet2', wb)
 sheet = wb.active
 
-i = 2
+i = 2521
 
-# with open('test.txt', 'w') as f:
-# 	while i <= 2527:
-# 		count = 0
-# 		for j in range(1, 11):
-# 			one = sheet[f'E{i}'].value.replace('-', '').replace('_', '') if not sheet[f'E{i}'].value.split('-')[-1].isdigit() else sheet[f'E{i}'].value[:-4].replace('-', '').replace('_', '')
-# 			two = sheet[f'E{i+j}'].value.replace('-', '').replace('_', '') if not sheet[f'E{i+j}'].value.split('-')[-1].isdigit() else sheet[f'E{i+j}'].value[:-4].replace('-', '').replace('_', '')
-# 			onearr = ''.join([i for i in [char for char in one] if i.isdigit()])
-# 			twoarr = ''.join([i for i in [char for char in two] if i.isdigit()])
-# 			if fuzz.token_sort_ratio(one, two) >= 80 and fuzz.token_sort_ratio(onearr, twoarr) >= 85 or fuzz.token_sort_ratio(onearr, twoarr) >= 90:
-# 				f.write(f"{i}: {sheet[f'E{i}'].value}\n")
-# 				print(i)
-# 				count += 1
-# 			else:
-# 				break
+with open('test.txt', 'w') as f:
+	while i <= 2527:
+		count = 1
 
-# 		if count > 0:
-# 			i += count
-# 		else:
-# 			i += 1
+		for j in range(1, 11):
+			one = sheet[f'E{i}'].value.replace('-', '').replace('_', '') if not sheet[f'E{i}'].value.split('-')[-1].isdigit() else sheet[f'E{i}'].value[:-4].replace('-', '').replace('_', '')
+			two = sheet[f'E{i+j}'].value.replace('-', '').replace('_', '') if not sheet[f'E{i+j}'].value.split('-')[-1].isdigit() else sheet[f'E{i+j}'].value[:-4].replace('-', '').replace('_', '')
+			onearr = ''.join([i for i in [char for char in one] if i.isdigit()])
+			twoarr = ''.join([i for i in [char for char in two] if i.isdigit()])
+			if fuzz.token_sort_ratio(one, two) >= 80 and fuzz.token_sort_ratio(onearr, twoarr) >= 85 or fuzz.token_sort_ratio(onearr, twoarr) >= 90:
+				f.write(f"{i}: {sheet[f'E{i}'].value}\n")
+				print(i)
+				count += 1
+			else:
+				break
+
+		i += count
 
 one = sheet[f'E52'].value.replace('-', '').replace('_', '') if not sheet[f'E52'].value.split('-')[-1].isdigit() else sheet[f'E52'].value[:-4].replace('-', '').replace('_', '')
 two = sheet[f'E53'].value.replace('-', '').replace('_', '') if not sheet[f'E53'].value.split('-')[-1].isdigit() else sheet[f'E53'].value[:-4].replace('-', '').replace('_', '')
