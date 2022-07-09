a92fc25edf01cab7609fba6b1b22934e94039afc
Carson <carlnod121@gmail.com>
2022-07-08 20:49:00 -0500

    almost done

--- categorize.py
+++ categorize.py
@@ -36,54 +36,104 @@ def main():
 
 	driver = webdriver.Chrome("chromedriver.exe")
 
-	for i in range(2, 50):
-		address = sheet[f"E{i}"].value
-		name = sheet[f"F{i}"].value.replace("&", "and")
+	i = 72
 
-		driver.get(f'https://maps.google.com/?q={name} {address}')
+	title = "/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[1]/h1/span[1]"
+	description = "PYvSYb"
+	services = "E0DTEd"
+	r_address = "rogA2c" 
+	type_restaurant = "button[jsan='7.DkEaL,0.jsaction']"
+	reviews = "div[role='radiogroup']"
 
-		title = "/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[1]/h1/span[1]"
-		description = "PYvSYb"
-		services = "E0DTEd"
-		address = "Io6YTe fontBodyMedium" 
-		type_restaurant = "DkEaL"
-		reviews = "m6QErb tLjsW"  
-		
-		def checks():
+	def checks(address, name):
 			types = dict.fromkeys(['thai', 'viet', 'india'], 'N')
 			types.update(dict.fromkeys(['buffet'], 'F'))
 			types.update(dict.fromkeys(['cajun', 'seafood'], 'I'))
 			types.update(dict.fromkeys(['wholesale'], 'W'))
-			types.update(dict.fromkeys(['sushi', 'japan'], 'L'))
+			types.update(dict.fromkeys(['sushi', 'japan', 'tokyo'], 'L'))
 			types.update(dict.fromkeys(['barbeque', 'bbq'], 'K'))
 			types.update(dict.fromkeys(['hot pot'], 'J'))
+			types.update(dict.fromkeys(['mexic'], 'Q'))
+			types.update(dict.fromkeys(['dine-in'], 'G'))
+			types.update(dict.fromkeys(['takeout, drive-through'], 'G'))
 
 
-			print(f'{i}: {driver.find_element("xpath", title).text}')
-			if "closed" in driver.find_element(By.CLASS_NAME, type_restaurant).text.lower():
+
+
+			try:
+				driver.find_element(By.CSS_SELECTOR, 'span[style="color:#D93025"]')
 				sheet[f"H{i}"] = "V"
+				print(f'{i}: {driver.find_element("xpath", title).text} and type is closed')
+				return 
+			except Exception as e:
+				pass
+
+
+			WebDriverWait(driver, timeout=1).until(lambda d: d.find_element(By.CSS_SELECTOR, type_restaurant))
+			restaurant_type = driver.find_element(By.CSS_SELECTOR, type_restaurant).text
+			restaurant_title = driver.find_element("xpath", title).text
+			WebDriverWait(driver, timeout=1).until(lambda d: d.find_element(By.CLASS_NAME, r_address))
+			restaurant_addr = driver.find_element(By.CLASS_NAME, r_address).text
+			restaurant_reviews = []
+
+			try:
+				restaurant_reviews = driver.find_element(By.CSS_SELECTOR, reviews).lower().split('\n')
+				print(restaurant_reviews)
+			except Exception as e:
+				pass
+
+			print(f'{i}: {restaurant_title} and type is {restaurant_type}')
+
+
+			one = address.replace('-', '').replace('_', '') if not sheet[f'E{i}'].value.split('-')[-1].isdigit() else sheet[f'E{i}'].value[:-4].replace('-', '').replace('_', '')
+			two = restaurant_addr
+
+			onearr = ''.join([k for k in [char for char in one] if k.isdigit()])
+			twoarr = ''.join([k for k in [char for char in two] if k.isdigit()])
+
+
+			if (fuzz.token_sort_ratio(one, two) < 70 and fuzz.token_sort_ratio(onearr, twoarr) < 85 and fuzz.partial_ratio(one.lower(), two.lower()) < 90):
+				sheet[f"H{i}"] = "T"
 				return
 
-			for i in ["wholesale", "thai", "viet", "india", "sushi", "japan", "barbeque", "bbq", "hot pot", "cajun", "seafood", "buffet"]:
-				if i in driver.find_element(By.CLASS_NAME, type_restaurant).text.lower() or i in driver.find_element("xpath", title).text.lower():
-					sheet[f"H{i}"] = types[i]
-					return
 
-			
+			try:
+				for keyword in ["wholesale", "thai", "viet", "india", "mexic", "sushi", "japan", "tokyo", "barbeque", "bbq", "hot pot", "cajun", "seafood", "buffet"]:
+					if keyword in restaurant_type.lower() or keyword in restaurant_title.lower() or len([s for s in restaurant_reviews if keyword in s]) != 0:
+						sheet[f"H{i}"] = types[keyword]
+						return
+			except Exception as e:
+				pass
 
+
+			try:
+				WebDriverWait(driver, timeout=1).until(lambda d: d.find_element(By.CLASS_NAME, services))
+				sheet[f"H{i}"] = types[driver.find_element(By.CLASS_NAME, services).text.lower().split('\n')[0]]
+			except Exception as e:
+				sheet[f"H{i}"] = "T"
+
+
+			return
+
+
+	while i <= 80:
+		address = sheet[f"E{i}"].value
+		name = sheet[f"F{i}"].value.replace("&", "and").replace("#", "")
+
+		driver.get(f'https://maps.google.com/?q={name} {address}')
 
 		try:
-			checks()
-
-		except NoSuchElementException:
+			checks(address, name)
+		except Exception as e:
 			try:
-				WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.CLASS_NAME, "hfpxzc"))
+				WebDriverWait(driver, timeout=2).until(lambda d: d.find_element(By.CLASS_NAME, "hfpxzc"))
 				driver.find_element(By.CLASS_NAME, "hfpxzc").click()
-				WebDriverWait(driver, timeout=3).until(lambda d: d.find_element("xpath", title))
-				print(f'{i}: {driver.find_element("xpath", title).text}')
-			except NoSuchElementException:
-				print(i)
-			
+				WebDriverWait(driver, timeout=2).until(lambda d: d.find_element("xpath", title))
+				checks(address, name)
+			except Exception as e:
+				print(f'{i}: error')
+		
+		i += 1
 
 
 
@@ -91,6 +141,7 @@ def main():
 			# print(f'{i}: {[(fuzz.token_sort_ratio(name, k), fuzz.partial_ratio(name.lower(), k.lower())) for k in result_names]}')
 			# print(result_names)
 
+	wb.save(filename = wb_file)
 
 
 if __name__ == "__main__":
